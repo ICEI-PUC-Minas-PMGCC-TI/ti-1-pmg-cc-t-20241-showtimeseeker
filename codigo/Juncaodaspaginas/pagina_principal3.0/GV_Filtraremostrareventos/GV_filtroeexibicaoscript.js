0f316e1bcde10fe7309c26f5608bc36b00649647
Gabriel3637 <121583913+Gabriel3637@users.noreply.github.com>
2024-06-29 11:45:21 -0300

    v1.22

--- codigo/Juncaodaspaginas/pagina_principal3.0/GV_Filtraremostrareventos/GV_filtroeexibicaoscript.js
+++ codigo/Juncaodaspaginas/pagina_principal3.0/GV_Filtraremostrareventos/GV_filtroeexibicaoscript.js
@@ -10,26 +10,90 @@ function GV_lerdadoseventodobancodedados()
 //Dados iniciais caso nao existam dados anteriores
                 objdados = {
                     evento:[
-                        {
-                                nome_do_evento: "",
-                                local: "",
+                            {
+                                    nome_do_evento: "",
+                                    local: "",
+                                    preco: {
+                                        valor: 0,
+                                        moeda: "R$"
+                                    },
+                                    data: "",
+                                    descricao: "",
+                                    fotos: [],
+                                    id: 0,
+                                    linkcontato: "",
+                                    estilodoevento: [],
+                                    visualizacoes: {
+                                        total: 0,
+                                        gosto1: 0,
+                                        gosto2: 0,
+                                        gosto3: 0,
+                                        gosto4: 0,
+                                        gosto5: 0,
+                                        gosto6: 0,
+                                        gosto7: 0,
+                                        gosto8: 0,
+                                        gosto9: 0,
+                                        gosto10: 0,
+                                        gosto11: 0,
+                                        gosto12: 0,
+                                    },
+                                    favoritos: 0,
+                                    comentarios: [],
+                                    donodoevento: "",
+                            },
+                            {
+                                nome_do_evento: "Pool party",
+                                local: "Avenida Jubeline",
+                                preco: {
+                                    valor: 50,
+                                    moeda: "R$"
+                                },
+                                data: "2024-07-01",
+                                descricao: "Em Maio temos mais um encontro marcado no Rancho Bill para comemorarmos o Bday do Felipe Arruda.\nUm super line, numa junção de muita vibe que o Rancho já tem, com a vibe que BH inteira já conhece das nossas festas. \nVai ser mais um DOMINGO de muita vibe, alegria e uma comemoração inesquecível.",
+                                fotos: ["https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F451776649%2F1408553967403%2F1%2Foriginal.20230222-065803?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C238%2C1080%2C540&s=d44826119d6bec5f86d3faf2729717d7", "https://hips.hearstapps.com/hmg-prod/images/701/pool-party1-1499900484.jpg"],
+                                id: 1718186424176,
+                                linkcontato: "https://www.instagram.com/",
+                                estilodoevento: ["2", "3"],
+                                visualizacoes: {
+                                    total: 0,
+                                    gosto1: 5,
+                                    gosto2: 90,
+                                    gosto3: 50000,
+                                    gosto4: 50,
+                                    gosto5: 1,
+                                    gosto6: 60,
+                                    gosto7: 600,
+                                    gosto8: 44,
+                                    gosto9: 9,
+                                    gosto10: 4,
+                                    gosto11: 89,
+                                    gosto12: 15,
+                                },
+                                favoritos: 0,
+                                comentarios: [],
+                                donodoevento: "",
+                            },
+                            {
+                                nome_do_evento: "Festa Junina",
+                                local: "Avenida Dom Pedro II",
                                 preco: {
                                     valor: 0,
                                     moeda: "R$"
                                 },
-                                data: "",
-                                descricao: "",
-                                fotos: [],
-                                id: 0,
-                                linkcontato: "",
-                                estilodoevento: [],
+                                data: "2024-08-19",
+                                descricao: "Venha se juntar a nós para celebrar a tradição e a alegria da Festa Junina do Rotary Club de Belo Horizonte - Barro Preto! No dia 8 de junho de 2024, a partir das 14h, o Colégio São Paulo, localizado na Rua Olímpio de Assis 190, Cidade Jardim, Belo Horizonte/MG, será o palco para essa festa cheia de diversão e cultura.",
+                                fotos: ["https://fashionistando.uai.com.br/wp-content/uploads/sites/11/2018/06/festa-junina-camarim.jpg", "https://soubh.uai.com.br/uploads/post/image/14267/festas_juninas_em_bh_ingressos_onde_ir.jpg"],
+                                id: 1718186421301,
+                                linkcontato: "https://www.instagram.com/",
+                                estilodoevento: ["1", "3"],
                                 visualizacoes: {
                                     total: 0,
-                                    gosto1: 0,
-                                    gosto2: 0,
-                                    gosto3: 0,
-                                    gosto4: 0,
-                                    gosto5: 0,
+                                    gosto1: 3,
+                                    gosto2: 10,
+                                    gosto3: 50,
+                                    gosto4: 1,
+                                    gosto5: 100,
                                     gosto6: 0,
                                     gosto7: 0,
                                     gosto8: 0,
@@ -40,110 +104,75 @@ function GV_lerdadoseventodobancodedados()
                                 },
                                 favoritos: 0,
                                 comentarios: [],
+                                donodoevento: 1719666789117,
+                            },
+                            {
+                                nome_do_evento: "KPOP BRASIL FEST",
+                                local: "Bairro Niteroi, Rio de Janeiro",
+                                preco: {
+                                    valor: 800,
+                                    moeda: "R$"
+                                },
+                                data: "2024-08-08",
+                                descricao: "Apresento a vocês nosso projeto K-pop Brasil Fest que vem com tudo no bairro de Niterói, em um novo local com um auditório belíssimo para confortar vocês que amam K-pop e cultura Coreana",
+                                fotos: ["https://f.i.uol.com.br/fotografia/2019/11/05/15729835355dc1d2efabe3b_1572983535_3x2_md.jpg", "https://f.i.uol.com.br/fotografia/2019/09/30/15698728845d925bf4d6793_1569872884_3x2_md.jpg"],
+                                id: 1718186419035,
+                                linkcontato: "https://www.instagram.com/",
+                                estilodoevento: ["1", "4"],
+                                visualizacoes: {
+                                    total: 0,
+                                    gosto1: 5,
+                                    gosto2: 0,
+                                    gosto3: 2,
+                                    gosto4: 0,
+                                    gosto5: 0,
+                                    gosto6: 0,
+                                    gosto7: 100,
+                                    gosto8: 0,
+                                    gosto9: 1,
+                                    gosto10: 7,
+                                    gosto11: 0,
+                                    gosto12: 0,
+                                },
+                                favoritos: 0,
+                                comentarios: [],
                                 donodoevento: "",
-                        },
-                        {
-                            nome_do_evento: "Pool party",
-                            local: "Avenida Jubeline",
-                            preco: {
-                                valor: 50,
-                                moeda: "R$"
                             },
-                            data: "2024-08-06",
-                            descricao: "Em Maio temos mais um encontro marcado no Rancho Bill para comemorarmos o Bday do Felipe Arruda.\nUm super line, numa junção de muita vibe que o Rancho já tem, com a vibe que BH inteira já conhece das nossas festas. \nVai ser mais um DOMINGO de muita vibe, alegria e uma comemoração inesquecível.",
-                            fotos: ["https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F451776649%2F1408553967403%2F1%2Foriginal.20230222-065803?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C238%2C1080%2C540&s=d44826119d6bec5f86d3faf2729717d7", "https://hips.hearstapps.com/hmg-prod/images/701/pool-party1-1499900484.jpg"],
-                            id: 1718186424176,
-                            linkcontato: "https://www.instagram.com/",
-                            estilodoevento: ["2", "3"],
-                            visualizacoes: {
-                                total: 0,
-                                gosto1: 7,
-                                gosto2: 90,
-                                gosto3: 50000,
-                                gosto4: 50,
-                                gosto5: 0,
-                                gosto6: 0,
-                                gosto7: 0,
-                                gosto8: 0,
-                                gosto9: 0,
-                                gosto10: 0,
-                                gosto11: 0,
-                                gosto12: 0,
-                            },
-                            favoritos: 0,
-                            comentarios: [],
-                            donodoevento: "",
-                        },
-                        {
-                            nome_do_evento: "Festa Junina",
-                            local: "Avenida Dom Pedro II",
-                            preco: {
-                                valor: 0,
-                                moeda: "R$"
-                            },
-                            data: "2024-08-19",
-                            descricao: "Venha se juntar a nós para celebrar a tradição e a alegria da Festa Junina do Rotary Club de Belo Horizonte - Barro Preto! No dia 8 de junho de 2024, a partir das 14h, o Colégio São Paulo, localizado na Rua Olímpio de Assis 190, Cidade Jardim, Belo Horizonte/MG, será o palco para essa festa cheia de diversão e cultura.",
-                            fotos: ["https://fashionistando.uai.com.br/wp-content/uploads/sites/11/2018/06/festa-junina-camarim.jpg", "https://soubh.uai.com.br/uploads/post/image/14267/festas_juninas_em_bh_ingressos_onde_ir.jpg"],
-                            id: 1718186421301,
-                            linkcontato: "https://www.instagram.com/",
-                            estilodoevento: ["1", "3"],
-                            visualizacoes: {
-                                total: 0,
-                                gosto1: 3,
-                                gosto2: 10,
-                                gosto3: 50,
-                                gosto4: 0,
-                                gosto5: 0,
-                                gosto6: 0,
-                                gosto7: 0,
-                                gosto8: 0,
-                                gosto9: 0,
-                                gosto10: 0,
-                                gosto11: 0,
-                                gosto12: 0,
-                            },
-                            favoritos: 0,
-                            comentarios: [],
-                            donodoevento: "",
-                        },
-                        {
-                            nome_do_evento: "KPOP BRASIL FEST",
-                            local: "Bairro Niteroi, Rio de Janeiro",
-                            preco: {
-                                valor: 800,
-                                moeda: "R$"
-                            },
-                            data: "2024-08-08",
-                            descricao: "Apresento a vocês nosso projeto K-pop Brasil Fest que vem com tudo no bairro de Niterói, em um novo local com um auditório belíssimo para confortar vocês que amam K-pop e cultura Coreana",
-                            fotos: ["https://f.i.uol.com.br/fotografia/2019/11/05/15729835355dc1d2efabe3b_1572983535_3x2_md.jpg", "https://f.i.uol.com.br/fotografia/2019/09/30/15698728845d925bf4d6793_1569872884_3x2_md.jpg"],
-                            id: 1718186419035,
-                            linkcontato: "https://www.instagram.com/",
-                            estilodoevento: ["1", "4"],
-                            visualizacoes: {
-                                total: 0,
-                                gosto1: 5,
-                                gosto2: 0,
-                                gosto3: 2,
-                                gosto4: 0,
-                                gosto5: 0,
-                                gosto6: 0,
-                                gosto7: 0,
-                                gosto8: 0,
-                                gosto9: 0,
-                                gosto10: 0,
-                                gosto11: 0,
-                                gosto12: 0,
-                            },
-                            favoritos: 0,
-                            comentarios: [],
-                            donodoevento: "",
-                        },
                     ],
-                    listadeusuarios:[],
-                    usuario:[]
-                };
+                    listadeusuarios:[
+                        {
+                            username: "Eduardo",
+                            senha: "12345",
+                            email: "fffffff@gmail.com",
+                            ID: 1719666789117,
+                            precoMedio: "564",
+                            gostos: ["1", "2", "3"],
+                            moeda: "R$",
+                            favoritos: [1718186424176],
+                            visualizou: []
+                        }
+                    ],
+                    usuario:{}
+            };
         }
         return(objdados);
+}
+
+function GV_idindexevento(GV_id)
+{
+    let GV_dadosdoeventobuscarid = GV_lerdadoseventodobancodedados().evento;
+    let GV_indexpararetornar = -1;
+    let GV_contador = 1;
+    do
+    {
+        if(GV_dadosdoeventobuscarid[GV_contador].id == GV_id)
+        {
+            GV_indexpararetornar = GV_contador;
+        }
+        GV_contador = GV_contador + 1;
+    }
+    while(GV_contador < GV_dadosdoeventobuscarid.length && GV_indexpararetornar == -1);
+    return(GV_indexpararetornar);
 }
 
 const GV_bd_STS = GV_lerdadoseventodobancodedados();
@@ -231,6 +260,8 @@ function isUserLoggedIn() {
 }
 
 function GV_codigo(){
+
+    GV_notificacaoevento();
     
     const GV_botaodofiltro = document.getElementById("GV_botaodofiltro");
     const GV_gastomedio = document.getElementById('GV_preco');
@@ -292,3 +323,70 @@ function ir_para_perfil(){
         location.href = "index.html";
     }
 }
+
+
+function GV_notificacaoevento(){
+    
+    let GV_lognotif = sessionStorage.getItem('logado');
+    if(true){
+        console.log('teste');
+        let GV_objeventonot = GV_lerdadoseventodobancodedados();
+        const favoritedEvents = GV_objeventonot.usuario.favoritos;
+        const notificationElement = document.getElementById('notification');
+        function isHoje(dataAlvo) {
+            // Obter a data atual
+            const hoje = new Date();
+        
+            // Converter a data alvo do formato 'aaaa-mm-dd' para um objeto Date
+            const [ano, mes, dia] = dataAlvo.split('-');
+            const dataAlvoDate = new Date(ano, mes - 1, dia); // Meses começam do 0 em JavaScript
+        
+            // Comparar os componentes da data (ano, mês, dia)
+            return hoje.getFullYear() === dataAlvoDate.getFullYear() &&
+                hoje.getMonth() === dataAlvoDate.getMonth() &&
+                hoje.getDate() === dataAlvoDate.getDate();
+        }
+
+        function faltamDoisDias(dataAlvo) {
+            // Obter a data atual
+            const hoje = new Date();
+            
+            // Converter a data alvo do formato 'aaaa-mm-dd' para um objeto Date
+            const [ano, mes, dia] = dataAlvo.split('-');
+            const dataAlvoDate = new Date(ano, mes - 1, dia); // Meses começam do 0 em JavaScript
+        
+            // Calcular a diferença em milissegundos
+            const diferencaMillis = dataAlvoDate - hoje;
+            
+            // Converter a diferença para dias
+            const diferencaDias = Math.ceil(diferencaMillis / (1000 * 60 * 60 * 24));
+        
+            // Verificar se a diferença é igual a 2
+            return diferencaDias === 2;
+        }
+
+        function showNotification(message) {
+            notificationElement.textContent = message;
+            notificationElement.classList.remove('hidden');
+            setTimeout(() => {
+                notificationElement.classList.add('hidden');
+            }, 5000); // Hide notification after 5 seconds
+        }
+
+        // Check for notifications on page load
+        favoritedEvents.forEach(event => {
+            let GV_event = GV_idindexevento(event)   
+            if(GV_event > 0 && GV_objeventonot.evento[GV_event].id == event)
+            {
+                if (faltamDoisDias(GV_objeventonot.evento[GV_event].data)) {
+                    console.log("Vai")
+                    showNotification(`Lembrete: ${GV_objeventonot.evento[GV_event].nome_do_evento} ocorrerá em 48 horas!`);
+                } else if (isHoje(GV_objeventonot.evento[GV_event].data)) {
+                    console.log("Nao vai")
+                    showNotification(`Lembrete: ${GV_objeventonot.evento[GV_event].nome_do_evento} é hoje!`);
+                }
+            } 
+               
+        });
+    }
+};
